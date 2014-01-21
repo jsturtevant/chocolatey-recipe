1c20ab474fd0a1c98282d9f29ff8b9aa6dc32b60
jsturtevant <jsturtevant@gmail.com>
2014-01-21 18:57:59 -0500

    added git and organized tools

--- chocolatey-recipe.bat
+++ chocolatey-recipe.bat
@@ -1,6 +1,14 @@
-cinst ilmerge
+rem source control tools
 cinst winmerge
+cinst git
+
+rem editors
 cinst WebStorm
 cinst notepadplusplus
+
+rem image tools
 cinst paint.net
+
+rem binary tools
 cinst dotPeek
+cinst ilmerge
